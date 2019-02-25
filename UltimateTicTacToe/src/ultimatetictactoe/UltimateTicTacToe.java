c66f40389290a5acda4b826946d127d6e9cb176b
schemabuoi <charyszczakg@gmail.com>
2019-02-25 10:32:29 +0100

    Update UltimateTicTacToe.java
    Fixing starting point of the application

--- UltimateTicTacToe/src/ultimatetictactoe/UltimateTicTacToe.java
+++ UltimateTicTacToe/src/ultimatetictactoe/UltimateTicTacToe.java
@@ -19,7 +19,7 @@ public class UltimateTicTacToe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ultimatetictactoe/gui/view/GameView.fxml"));
         
         Scene scene = new Scene(root);
         
