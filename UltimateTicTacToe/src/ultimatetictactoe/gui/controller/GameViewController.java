d4b7f16687e094b8fe520ab24f573e8e3987ad7e
schemabuoi <charyszczakg@gmail.com>
2019-02-25 10:27:16 +0100

    Initializing NetBeans project
    Initializing NetBeans project + moving templates from teacher to bll package

---
+++ UltimateTicTacToe/src/ultimatetictactoe/gui/controller/GameViewController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimatetictactoe.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Acer
+ */
+public class GameViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
