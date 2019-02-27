602e785a5abbf7e946b78e6faa7e1dbfdc81bcc8
Nedas <nedassurkus@gmail.com>
2019-02-27 12:36:39 +0100

    Update to GameManager

--- UltimateTicTacToe/src/ultimatetictactoe/bll/game/GameManager.java
+++ UltimateTicTacToe/src/ultimatetictactoe/bll/game/GameManager.java
@@ -4,26 +4,26 @@ import ultimatetictactoe.bll.bot.IBot;
 import ultimatetictactoe.bll.move.IMove;
 
 /**
- * This is a proposed GameManager for Ultimate Tic-Tac-Toe,
- * the implementation of which is up to whoever uses this interface.
- * Note that initializing a game through the constructors means
- * that you have to create a new instance of the game manager 
- * for every new game of a different type (e.g. Human vs Human, Human vs Bot or Bot vs Bot),
- * which may not be ideal for your solution, so you could consider refactoring
- * that into an (re-)initialize method instead.
+ * This is a proposed GameManager for Ultimate Tic-Tac-Toe, the implementation
+ * of which is up to whoever uses this interface. Note that initializing a game
+ * through the constructors means that you have to create a new instance of the
+ * game manager for every new game of a different type (e.g. Human vs Human,
+ * Human vs Bot or Bot vs Bot), which may not be ideal for your solution, so you
+ * could consider refactoring that into an (re-)initialize method instead.
+ *
  * @author mjl
  */
 public class GameManager {
-    
+
     /**
      * Three different game modes.
      */
-    public enum GameMode{
+    public enum GameMode {
         HumanVsHuman,
         HumanVsBot,
         BotVsBot
     }
-    
+
     private final IGameState currentState;
     private int currentPlayer = 0; //player0 == 0 && player1 == 1
     private GameMode mode = GameMode.HumanVsHuman;
@@ -31,21 +31,23 @@ public class GameManager {
     private IBot bot2 = null;
 
     /**
-     * Set's the currentState so the game can begin.
-     * Game expected to be played Human vs Human
-     * @param currentState Current game state, usually an empty board, 
-     * but could load a saved game.
+     * Set's the currentState so the game can begin. Game expected to be played
+     * Human vs Human
+     *
+     * @param currentState Current game state, usually an empty board, but could
+     * load a saved game.
      */
     public GameManager(IGameState currentState) {
         this.currentState = currentState;
         mode = GameMode.HumanVsHuman;
     }
-    
+
     /**
-     * Set's the currentState so the game can begin.
-     * Game expected to be played Human vs Bot
-     * @param currentState Current game state, usually an empty board, 
-     * but could load a saved game.
+     * Set's the currentState so the game can begin. Game expected to be played
+     * Human vs Bot
+     *
+     * @param currentState Current game state, usually an empty board, but could
+     * load a saved game.
      * @param bot The bot to play against in vsBot mode.
      */
     public GameManager(IGameState currentState, IBot bot) {
@@ -53,12 +55,13 @@ public class GameManager {
         mode = GameMode.HumanVsBot;
         this.bot = bot;
     }
-    
+
     /**
-     * Set's the currentState so the game can begin.
-     * Game expected to be played Bot vs Bot
-     * @param currentState Current game state, usually an empty board, 
-     * but could load a saved game.
+     * Set's the currentState so the game can begin. Game expected to be played
+     * Bot vs Bot
+     *
+     * @param currentState Current game state, usually an empty board, but could
+     * load a saved game.
      * @param bot The first bot to play.
      * @param bot2 The second bot to play.
      */
@@ -68,77 +71,79 @@ public class GameManager {
         this.bot = bot;
         this.bot2 = bot2;
     }
-    
+
     /**
      * User input driven Update
+     *
      * @param move The next user move
      * @return Returns true if the update was successful, false otherwise.
      */
-    public Boolean updateGame(IMove move)
-    {
+    public Boolean updateGame(IMove move) {
         //Verify the new move
-        if(!verifyMoveLegality(move)) 
-        { 
-            return false; 
+        if (!verifyMoveLegality(move)) {
+            return false;
         }
-        
+
         //Update the currentState
         updateBoard(move);
         updateMacroboard(move);
-        
+
         //Update currentPlayer
         currentPlayer = (currentPlayer + 1) % 2;
-        
+
         return true;
     }
-    
+
     /**
      * Non-User driven input, e.g. an update for playing a bot move.
+     *
      * @return Returns true if the update was successful, false otherwise.
      */
-    public Boolean updateGame()
-    {
+    public Boolean updateGame() {
         //Check game mode is set to one of the bot modes.
-        assert(mode != GameMode.HumanVsHuman);
-        
+        assert (mode != GameMode.HumanVsHuman);
+        IMove botMove;
         //Check if player is bot, if so, get bot input and update the state based on that.
-        if(mode == GameMode.HumanVsBot && currentPlayer == 1)
-        {
+        if (mode == GameMode.HumanVsBot && currentPlayer == 1) {
             //Check bot is not equal to null, and throw an exception if it is.
-            assert(bot != null);
-            
-            IMove botMove = bot.doMove(currentState);
-            
+            assert (bot != null);
+
+            botMove = bot.doMove(currentState);
+
             //Be aware that your bots might perform illegal moves.
             return updateGame(botMove);
         }
-        
-        //Check bot is not equal to null, and throw an exception if it is.
-        assert(bot != null);
-        assert(bot2 != null);
-        
-        //TODO: Implement a bot vs bot Update.
-        throw new UnsupportedOperationException("Not supported yet."); 
+        if (mode == GameMode.BotVsBot) {
+            //Check bot is not equal to null, and throw an exception if it is.
+            assert (bot != null);
+            assert (bot2 != null);
+
+            //TODO: Implement a bot vs bot Update.
+            if (currentPlayer == 1) {
+                botMove = bot.doMove(currentState);
+            } else {
+                botMove = bot2.doMove(currentState);
+            }
+            return updateGame(botMove);
+        }
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private Boolean verifyMoveLegality(IMove move)
-    {
+
+    private Boolean verifyMoveLegality(IMove move) {
         //Test if the move is legal   
         //NOTE: should also check whether the move is placed on an occupied spot.
         System.out.println("Checking move validity against macroboard available field");
         System.out.println("Not currently checking move validity actual board");
         return currentState.getField().isInActiveMicroboard(move.getX(), move.getY());
     }
-    
-    private void updateBoard(IMove move)
-    {
-       //TODO: Update the board to the new state 
-        throw new UnsupportedOperationException("Not supported yet."); 
+
+    private void updateBoard(IMove move) {
+        //TODO: Update the board to the new state 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    private void updateMacroboard(IMove move)
-    {
-       //TODO: Update the macroboard to the new state 
-       throw new UnsupportedOperationException("Not supported yet."); 
+
+    private void updateMacroboard(IMove move) {
+        //TODO: Update the macroboard to the new state 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
