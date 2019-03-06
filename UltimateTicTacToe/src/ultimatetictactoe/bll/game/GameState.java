d3383ccc9895a03543904eeaf1aefb8e3b379286
schemabuoi <charyszczakg@gmail.com>
2019-03-06 16:45:07 +0100

    Update GameState.java
    Implementing methods for rounds and moves

--- UltimateTicTacToe/src/ultimatetictactoe/bll/game/GameState.java
+++ UltimateTicTacToe/src/ultimatetictactoe/bll/game/GameState.java
@@ -14,8 +14,26 @@ import ultimatetictactoe.bll.field.IField;
  */
 public class GameState implements IGameState{
     
-    IField field = new Field();
+    IField field;
+    int moveNumber;
+    int roundNumber;
 
+    public GameState()
+    {
+        field = new Field();
+        moveNumber=0;
+        roundNumber=0;
+    }
+    
+    public GameState(IGameState state) {
+        field = new Field();
+        field.setMacroboard(state.getField().getMacroboard());
+        field.setBoard(state.getField().getBoard());
+
+        moveNumber = state.getMoveNumber();
+        roundNumber = state.getRoundNumber();
+    }
+    
     @Override
     public IField getField() {
         return field;
@@ -23,22 +41,22 @@ public class GameState implements IGameState{
 
     @Override
     public int getMoveNumber() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return moveNumber;
     }
 
     @Override
     public void setMoveNumber(int moveNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.moveNumber = moveNumber;
     }
 
     @Override
     public int getRoundNumber() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return roundNumber;
     }
 
     @Override
     public void setRoundNumber(int roundNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.roundNumber = roundNumber;
     }
     
 }
